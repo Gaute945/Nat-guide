6488d8c8694f9bd6d79239347f03a716f2768316
Gaute <mrboobai@proton.me>
2023-11-23 20:55:54 +0100

    quiz working til q3, questions filled in

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-function next(questionNumId) {
+let score = 0; // Declare score outside the function
+
+function next(questionNumId, qid) {
     let DOMqDiv = document.getElementById(questionNumId);
     let NextQuestionNumId = questionNumId + 1;
-    let NextDOMQDiv = document.getElementById(NextQuestionNumId);
+    if (questionNumId === 5) {
+        // If questionNumId is 4, you can return or do nothing to exit the function
+        return;
+    } else {
+        let NextDOMQDiv = document.getElementById(NextQuestionNumId);
 
-    DOMqDiv.classList.add("hidden");
-    NextDOMQDiv.classList.remove("hidden");
+        DOMqDiv.classList.add("hidden");
+        NextDOMQDiv.classList.remove("hidden");
+    }
+
+    let qx = document.getElementById(qid);
+
+    if (qx.checked) {
+        console.log("it worked!");
+        score++;
+    } else {
+        console.log("it failed");
+    }
+
+    console.log(score);
 }
